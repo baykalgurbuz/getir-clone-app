0c8ace49608a4f48f2714b041abaf697db08d5d0
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-08-08 16:02:50 +0300

    Slider components added in this project

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,33 @@
-import React from 'react'
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 export default function HeroSection() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+    
   return (
-    <div>HeroSection</div>
+    <div className="relative h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
+          <Slider {...settings}>
+          <div>
+            <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-2-7c23764275cdaf14d7b6cf15ebbdd0c1.jpg" alt="" />
+          </div>
+          <div>
+            <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-3-ffd1e98c5da51809f9adcc3a4a37d751.jpg" alt=""/>
+          </div>
+          <div>
+            <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg" alt="" />
+          </div>
+        </Slider>
+        <div className="container flex justify-between items-center absolute top-0 right-1/2 translate-x-1/2 h-full z-20">
+            <div>
+              <img src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg" alt=""/>
+            </div>
+        </div>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor:theme=>({
-        'brand-color':'#5d3ebc'
-      })
+        'brand-color':'#4c3398',
+        'primary-brand-color':'#5d3ebc',
+        'secondary-brand-color':'#7849f7'
+      }),
+      gradientColorStops:theme=>({
+        'brand-color':'#4c3398',
+        'primary-brand-color':'#5d3ebc',
+        'secondary-brand-color':'#7849f7'
+      }),
+      
     },
   },
   variants: {
