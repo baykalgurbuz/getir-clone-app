e7735300faad446ea58ad40196b659374d44bf50
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-08-08 18:38:14 +0300

    slider completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundColor:theme=>({
+      colors:{
         'brand-color':'#4c3398',
         'primary-brand-color':'#5d3ebc',
-        'secondary-brand-color':'#7849f7'
-      }),
-      gradientColorStops:theme=>({
-        'brand-color':'#4c3398',
-        'primary-brand-color':'#5d3ebc',
-        'secondary-brand-color':'#7849f7'
-      }),
-      
+        'secondary-brand-color':'#7849f7',
+        'brand-yellow':'#ffd300'
+      },
+  
     },
   },
   variants: {
