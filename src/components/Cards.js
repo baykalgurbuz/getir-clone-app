b2442287acfae76c11a479757b576a92c3b16b16
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-08-10 00:04:08 +0300

    last changed  done

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,7 +10,7 @@ export default function Cards() {
   },[])
   
   return (
-    <div className=' grid grid-cols-3 gap-x-4 container mx-auto'>
+    <div className=' grid grid-cols-3 gap-x-4 container mx-auto mt-8'>
       {cards.length && cards.map(card=>(
         <div className='bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center'>
         <img src={card.image} className="mb-6" alt=""/>
