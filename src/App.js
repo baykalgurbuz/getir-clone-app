af42fa5aa9b220ffb42f7a96a6f1778d803e33c2
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-08-09 16:13:18 +0300

    Mobile App component completed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
- 
-import Campaings from './components/Campaings'
-import Cards from './components/Cards'
-import Categories from './components/Categories'
-import Favorites from './components/Favorites'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import HeroSection from './components/HeroSection'
-import MobileApp from './components/MobileApp'
-
-
+import Campaings from "./components/Campaings";
+import Cards from "./components/Cards";
+import Categories from "./components/Categories";
+import Favorites from "./components/Favorites";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import MobileApp from "./components/MobileApp";
 
 function App() {
   return (
     <>
-    <Header/>
-    <HeroSection/>
-    <Categories/>  
-      
-     <Campaings/>
-    <Favorites/>
- 
-    <Cards/>
- 
-    <MobileApp/>
-    <Footer/>
+      <Header />
+      <HeroSection />
+      <Categories />
+
+      <Campaings />
+      <Favorites />
+      <MobileApp />
+      <Cards />
+
+      <Footer />
     </>
   );
 }
--- src/components/MobileApp.js
+++ src/components/MobileApp.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function MobileApp() {
   return (
-    <div>MobileApp</div>
-  )
+    <div className="container mx-auto flex items-center justify-between bg-primary-brand-color bg-mobile-app mt-7 rounded-lg text-white">
+      <div className="flex flex-col gap-y-3 p-10">
+        <h3 className="text-2xl font-bold tracking-tight  ">Getiri İndirin</h3>
+        <p className="font-semibold">İstediğiniz ürünleri dakikalar içinde kapınıza <br/> getirelim</p>
+        <nav className="mt-5 flex gap-x-2">
+          <a href="www.getir.com" className="transition-all hover:scale-105 transform">
+            <img src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg" alt="" />
+          </a>
+          <a href="www.getir.com" className="transition-all hover:scale-105 transform">
+            <img src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg" alt=""  />
+          </a> 
+          <a href="www.getir.com" className="transition-all hover:scale-105 transform">
+            <img src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg" alt="" />
+          </a>
+        </nav>
+      </div>
+      <picture className="pt-6">
+        <img src="https://getir.com/_next/static/images/phoneLanding-88c033545710c4808054072689e187d7.png" alt=""/>
+      </picture>
+    </div>
+  );
 }
